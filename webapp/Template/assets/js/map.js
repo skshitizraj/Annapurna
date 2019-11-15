96fc842eb17272ffc312a320091cb966ef8909ba
subashtiwari1010 <subashtiwari1010@gmail.com>
2019-11-15 20:35:01 +0545

    added label

--- webapp/Template/assets/js/map.js
+++ webapp/Template/assets/js/map.js
@@ -2,33 +2,31 @@ $(document).ready(function () {
     console.log("sdfasd");
 
     var map = L.map('mapid',{
-        minZoom:8,
+        minZoom:10.5,
     });
-    map.setView([28.2957487, 83.8123341], 8);
+    map.setView([28.2957487, 83.8123341], 10.5);
 
     osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     });
                 
-    googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+    var googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
         maxZoom: 20,
         subdomains:['mt0','mt1','mt2','mt3']
-    });
+    }).addTo(map);
     googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
         maxZoom: 20,
         subdomains:['mt0','mt1','mt2','mt3']
     });
-    var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+    googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
         maxZoom: 20,
         subdomains:['mt0','mt1','mt2','mt3']
-    }).addTo(map);
+    });
     googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
         maxZoom: 20,
         subdomains:['mt0','mt1','mt2','mt3']
     });
     mapboxTiles = L.tileLayer('https://api.mapbox.com/styles/v1/skshitiz1/cjvosths00oqu1cln1v7765pf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2tzaGl0aXoxIiwiYSI6ImNqcmJ2czBjODBhMTgzeWxwM2t1djJuaXUifQ.wlFktg-soH3B_pqVyJj2Ig')
-    
-    // var mapboxTiles = L.tileLayer('https://api.mapbox.com/styles/v1/skshitiz1/cjvosths00oqu1cln1v7765pf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2tzaGl0aXoxIiwiYSI6ImNqcmJ2czBjODBhMTgzeWxwM2t1djJuaXUifQ.wlFktg-soH3B_pqVyJj2Ig').addTo(map);
     
     var baseLayers = {
                     "OpenStreetMap": osm,
@@ -72,7 +70,8 @@ $(document).ready(function () {
                     });
                 }
             });
-         }
+            layer.bindTooltip(String(feature.properties.new_ward_n), {permanent:true,direction:'center',className: 'countryLabel'});
+        }
         map.fitBounds(dataLayer.getBounds());
       });
     // var marker = L.marker([28.2096, 83.9856]).addTo(map);
