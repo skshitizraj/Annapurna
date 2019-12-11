30eb640c112b80832ba3ed43ee71f72efd4b8849
skshitizraj <skshitizraj@gmail.com>
2019-12-11 15:00:51 +0545

    added cancel onclick , and button change

--- webapp/Template/assets/js/map.js
+++ webapp/Template/assets/js/map.js
@@ -1,4 +1,8 @@
+function cancelfunction() {
+    console.log("i am cancel");
+  }
 $(document).ready(function () {
+    
     // console.log("sdfasd");
 
     var map = L.map('mapid',{
@@ -27,40 +31,38 @@ $(document).ready(function () {
         maxZoom: 20,
         subdomains:['mt0','mt1','mt2','mt3']
     });
-    mapboxTiles = L.tileLayer('https://api.mapbox.com/styles/v1/skshitiz1/cjvosths00oqu1cln1v7765pf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2tzaGl0aXoxIiwiYSI6ImNqcmJ2czBjODBhMTgzeWxwM2t1djJuaXUifQ.wlFktg-soH3B_pqVyJj2Ig')
-    
-    var printProvider = L.print.Provider({
-        method: 'GET',
-        url: ' http://path/to/mapfish/print',
-        autoLoad: true,
-        dpi: 90
-     });
-     
-     var printControl = L.control.print({
-        provider: printProvider
-     });        
-     map.addControl(printControl);
-       
-
-
-
-
+    // mapboxTiles = L.tileLayer('https://api.mapbox.com/styles/v1/skshitiz1/cjvosths00oqu1cln1v7765pf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2tzaGl0aXoxIiwiYSI6ImNqcmJ2czBjODBhMTgzeWxwM2t1djJuaXUifQ.wlFktg-soH3B_pqVyJj2Ig')
     var baseLayers = {
                     "OpenStreetMap": osm,
                     "Google Streets": googleStreets,
                     "Google Hybrid": googleHybrid,
                     "Google Satellite": googleSat,
                     "Google Terrain": googleTerrain,
-                    "Mapbox Tiles": mapboxTiles
+                    // "Mapbox Tiles": mapboxTiles
                 };
                 
     layerswitcher = L.control.layers(baseLayers, {}, {collapsed: true}).addTo(map);
-    // const ee = require('@google/earthengine');
-    // ee.data.authenticateViaPrivateKey('data/privatekey.json');
-    // ee.initialize();
 
+    
+    //to set button selected 
+    $(function(){
+  
+        $(".dropdown-menu a").click(function(){
+            console.log(this);
+            $(this).parents(".dropdown").find('.btn').text($(this).text());
+        });
+      
+      });
+
+    //now to get json from api
     $.getJSON("http://127.0.0.1:8000/api/ward/all", function (data) {
-        // console.log('from');
+        console.log('from');
+        var area=0.000;
+        // for( var i=0 ; i<data.length;i++){
+        //     var area = 0;
+        //     area=area+feature[i].properties.area_sqkm;
+        //     console.log('FROM FOR');
+        // }
         var dataLayer=L.geoJson(data,{
             style:{
                 fillColor: " #172f76 ",
@@ -70,6 +72,9 @@ $(document).ready(function () {
                 weight: 2,
             },
             onEachFeature:function(feature, layer) {
+                
+                area = area + parseFloat(feature.properties.area_sqkm);
+                console.log(area);
                 layer.on({
                     mouseover: function () {
                         this.setStyle({
@@ -94,13 +99,14 @@ $(document).ready(function () {
                             var table=document.createElement("Table");
                             var tableOuter=document.createElement("Tableouter");
                             var crossmarker=document.createElement("i");
+                            tableOuter.setAttribute("class","card-body d-table");
                             crossmarker.setAttribute("class","la la-close");
                             crossmarker.setAttribute("id","close");
-                            tableOuter.setAttribute("class","card-body d-table");
                             tableContent.setAttribute("class","card border rounded");
                             tableTitle.setAttribute("class","card-header text-white backgroundcolourcss h5");
                             var text1 = document.createTextNode("Annapurna RM");
                             tableTitle.appendChild(text1);
+                            // tableTitle.appendChild(crossmarker);
                             table.setAttribute("class","table-responsive table-borderless table table-hover fontSize");
     
                             table.innerHTML+="<table><thead class=\"text-left\"><tr></tr></thead><tbody><tr><td>Ward Number</td><td>"+feature.properties.new_ward_n+"</td></tr><tr><td>District</td><td>"+feature.properties.district+"</td></tr><tr><td>Area (sq.km.)</td><td>"+feature.properties.area_sqkm+"</td></tr></tbody></table>";
@@ -108,7 +114,9 @@ $(document).ready(function () {
                             tableContent.appendChild(tableTitle);
                             tableOuter.appendChild(table);
                             tableContent.appendChild(tableOuter);
-                            document.getElementById("popup").appendChild(crossmarker);
+                            var x=document.getElementById("popup");
+                            x.appendChild(crossmarker);
+                            tableTitle.appendChild(crossmarker);
                             document.getElementById("popup").appendChild(tableContent);
 
                             document.getElementById("close").onclick=function(){
@@ -119,6 +127,8 @@ $(document).ready(function () {
                         }
                     }
                 });
+                var maintable= document.getElementById("maintable");
+                maintable.innerHTML="<table><thead class=\"text-left\"><tr></tr></thead><tbody><tr><td>District</td><td>"+feature.properties.district+"</td></tr><tr><td>Area (sq.km.)</td><td>"+area+"</td></tr></tbody></table>";
                 layer.bindTooltip(String(feature.properties.new_ward_n), {permanent:true,direction:'center',className: 'wardClassName'});
             },
         }).addTo(map);
